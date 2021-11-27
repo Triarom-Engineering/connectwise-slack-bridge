9dd593b2be963b7eaa755f1b043d1c4eca6cf99a
Cameron Fleming <cameron@nevexo.space>
2021-11-27 19:15:01 +0000

    Inital code commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # connectwise-slack-bridge
 A simple bridge tool that recieves messages from ConnectWise Control "new message" triggers and sends them into a Slack channel.
+
+## Installation
+
+It's recommended that this bridge tool is run under docker.
+
+Build the image with `docker build -t connectwise-slack .`
+
+### Docker Compose
+
+The docker-compose.yaml file contains the required env variables
+and image location for local building (src/)
+
+Run `docker-compose up -d` to launch the bridge
+
+### Manual Docker
+
+Environment Variables:
+
+- LOG_LEVEL: logging level `trace/debug/info/warn/error` (default info)
+- LISTEN_PORT: http port to listen on (default 3000)
+- SLACK_WEBHOOK: **REQUIRED** - Slack Webhook URL.
+
+`docker run -e LOG_LEVEL=info -e LISTEN_PORT=3000 -e SLACK_WEBHOOK=<webhook url> -p 3000:3000 --restart=unless-stopped connectwise-slack`
+
+### With Node
+
+This script can be run on any machine with Node installed, install the required
+dependencies first with `npm install` and then use `node index.js` to launch the program.
+
+## ConnectWise Control Trigger
+
+A trigger must be created in ConnectWise control with the following settings:
+
+**Event Filter**
+```
+Event.EventType = 'SentMessage' AND Connection.ProcessType = 'Guest' AND Session.HostConnectedCount = 0
+```
+
+**HTTP Method**
+```
+POST
+```
+
+**Content Type**
+```
+Application/JSON
+```
+
+**Body**
+```
+{*:json}
+```
+
+## Operation
+
+Once started, this tool runs as a daemon and listens for 
---
+++ src/index.js
@@ -0,0 +1,65 @@
+// Triarom Computers - ScreenConnect -> Slack Bridge
+
+const LISTEN_PORT = process.env.LISTEN_PORT || 3000
+const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK
+
+// Logging
+const log = require('pino')();
+log.level = process.env.LOG_LEVEL || 30;
+
+log.debug(`configured webhook url: ${SLACK_WEBHOOK}`)
+
+// Setup Express
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+// Use JSON handler
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+	// default handler
+	log.debug(`request: ${req.method} ${req.originalUrl} (remote: ${req.hostname})`);
+	next();
+})
+
+app.post("/alert", async (req, res) => {
+	// Main handler for alerts from ConnectWise Control Trigger
+
+	// Check for JSON payload
+	if (!req.body) {
+		log.warn(`dropping failed request, missing body.`);
+		return res.sendStatus(500);
+	}
+
+	// Check if payload is valid
+	if (!req.body.Event || !req.body.Session) {
+		log.warn(`dropping failed request, missing "Event" or "Session" fields.`);
+		return res.sendStatus(500);
+	}
+
+	// Fire the webhook
+	const body = req.body;
+	log.info(`successful request, forwarding to slack - ${body.Session.Name} - ${body.Event.Data}`);
+
+	await axios.post(SLACK_WEBHOOK, {
+		"text": `New message on session \`${body.Session.Name}\` - no engineers are currently connected to this guest. 
+		\nMessage:
+		\`${body.Event.Data}\``
+	}).catch(err => {
+		log.error(`request failed, error: ${err}`);
+	})
+})
+
+// Entrypoint
+const main = async () => {
+	if (SLACK_WEBHOOK == "<webhook url>") {
+		return log.fatal("Missing Slack webhook URL, please configure properly!")
+	}
+
+	app.listen(LISTEN_PORT)
+	log.info("Started Triarom Engineering ConnectWise Control - Slack Bridge, version 1.0, port: " + LISTEN_PORT);
+	log.debug("Debug logging enabled.")
+}
+
+main();
