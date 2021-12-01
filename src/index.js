d3f7fd7ff099492429974619c2b64974192d9357
Cameron Fleming <cameron@nevexo.space>
2021-12-01 19:13:33 +0000

    Add message inhibitors
    If a message is recieved from the same session, it will be dropped - useful if a conversation happens on ScreenConnect when an engineer is not remoted into the PC

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 const LISTEN_PORT = process.env.LISTEN_PORT || 3000
 const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK || "<webhook url>"
+const INHIBIT_REPEATS = (process.env.ENABLE_INHIBIT == "true" ? true : false) || true
+const INHIBIT_TIME = 1000 * 60 * (process.env.INHIBIT_TIME || 30)
 
 // Logging
 const log = require('pino')();
@@ -13,6 +15,50 @@ log.debug(`configured webhook url: ${SLACK_WEBHOOK}`)
 const express = require("express");
 const axios = require("axios");
 const app = express();
+
+// Message Inhibit
+const cron = require("node-cron");
+let inhibits = [];
+
+const register_inhibit = (company, session_name) => {
+	inhibits.push({
+		company: company,
+		session_name: session_name,
+		time: Date.now()
+	})
+
+	log.debug("inhibit registered for company: " + company + " session: " + session_name);
+}
+
+const is_inhibited = (company, session_name) => {
+	// Checks for inhibited messages.
+	// Inhibits are raised when a company+computer sends a message, inhibits last
+	// INHIBIT_TIME minutes, another message will not be forwarded until the inhibit lifts.
+	// Both the company name & session_name must match.
+	
+	for (const inhibit of inhibits) {
+		if (inhibit.company == company && inhibit.session_name == session_name) {
+			log.debug("inhibit exists for company: " + company + " session: " + session_name);
+			
+			// Check date on this inhibit
+			const release_time = inhibit.time + INHIBIT_TIME;
+			log.trace(`release inhibit at: ${new Date(release_time).toISOString()}, time now: ${new Date().toISOString()}`);
+			
+			if (Date.now() > release_time) {
+				log.debug("inhibit has expired, dropping.");
+				log.trace("inhibit clear, " + inhibits.indexOf(inhibit) + " inhibits: " + inhibits.length);
+				const slice = inhibits.splice(inhibits.indexOf(inhibit), 1);
+				log.trace(slice)
+				log.trace("after slice size: " + inhibits.length)
+				return false;
+			}
+
+			// Inhibit is still in force
+			log.debug("inhibit has triggered! The next message will drop.");
+			return true;
+		}
+	}
+}
 
 // Use JSON handler
 app.use(express.json());
@@ -56,6 +102,19 @@ app.post("/alert", async (req, res) => {
 
 	log.debug(message);
 
+	// Check if this company+session has an inhibit.
+	if (INHIBIT_REPEATS && is_inhibited(body.Session.CustomProperty1 || "SUPPORT_SESSION", body.Session.Name)) {
+		return log.info("this message is inhibited due to a previous message - dropping.");
+	}
+	
+	// Register the inhibit
+	if (INHIBIT_REPEATS) register_inhibit(body.Session.CustomProperty1 || "SUPPORT_SESSION", body.Session.Name);
+
+	// Respond to webhook.
+	res.sendStatus(200);
+
+	if (process.env.DISABLE_HOOK == "true") return log.warn("hook disabled with DISABLE_HOOK environment variable, dropping.");
+
 	await axios.post(SLACK_WEBHOOK, {
 		"text":  message
 	}).catch(err => {
@@ -66,12 +125,18 @@ app.post("/alert", async (req, res) => {
 // Entrypoint
 const main = async () => {
 	if (SLACK_WEBHOOK == "<webhook url>") {
-		return log.fatal("Missing Slack webhook URL, please configure properly!")
+		return log.fatal("Missing Slack webhook URL, please configure properly!");
 	}
 
-	app.listen(LISTEN_PORT)
+	app.listen(LISTEN_PORT);
 	log.info("Started Triarom Engineering ConnectWise Control - Slack Bridge, version 1.0, port: " + LISTEN_PORT);
-	log.debug("Debug logging enabled.")
+	log.debug(`debug mode on, INHIBIT_REPEATS: ${INHIBIT_REPEATS}, INHIBIT_TIME: ${INHIBIT_TIME}ms`);
+
+	// Create inhibit cleanup
+	cron.schedule("0 0 * * *", () => {
+		log.info(`clearing ${inhibits.length} inhibits.`)
+		inhibits = [];
+	});
 }
 
 main();
